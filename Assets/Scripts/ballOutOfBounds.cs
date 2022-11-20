9c11b0d660139dd80ce062fc9251e6e1e8c85965
denuden <79180351+denuden@users.noreply.github.com>
2022-11-20 22:25:33 +0800

    SpawnFix- Ball Physics
    -Changed ball physic material dynamic friction from 0.6 to 0
    - ballOutofBounds Script added changed

--- Assets/Scripts/ballOutOfBounds.cs
+++ Assets/Scripts/ballOutOfBounds.cs
@@ -8,12 +8,20 @@ public class ballOutOfBounds : MonoBehaviour
     public Rigidbody Ball;
     public float x, y, z;
     private bool isSpawning;
+    private bool velocityDecreaseStopper = false;
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.y < bottomLimit) // if it falls
         {
+            if (Ball.name == "ball") // if the ball falls, then handle velocity
+            {
+                velocityDecreaseStopper = true;
+            }
+            Ball.angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);  // remove spinning
+
+
             Ball.rotation = Quaternion.identity;  // default rotation
             Ball.velocity = new Vector3(0, -10, 0); //  fixed velocity every spawn, to not retain fall velocity everytime it falls
             Ball.position = new Vector3(x, y, z); // default position
@@ -28,6 +36,30 @@ public class ballOutOfBounds : MonoBehaviour
             {
                 isSpawning = false;
             }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //continually decrease velocity
+
+        //if true
+        //, then continue to decrease
+        if (velocityDecreaseStopper)
+        {
+            Debug.Log("RUN " + velocityDecreaseStopper);
+            Ball.velocity = Ball.velocity * 0.9969f;
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if collided with terrain, therefore it already bounced once and doesnt need
+        // velocity decrease
+        if (collision.gameObject.name == "Terrain")
+        {
+            velocityDecreaseStopper = false;
+            Debug.Log("here =========" + velocityDecreaseStopper);
         }
     }
 }
