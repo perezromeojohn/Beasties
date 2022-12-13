c480b091e743c1d50d981ed472144072f5ec3c30
Romeo John Perez <201911621@gordoncollege.edu.ph>
2022-12-13 22:25:45 +0800

    BEASTIES FIRST OFFICIAL RELEASE AGAIN

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -16,15 +16,28 @@ public class Settings : MonoBehaviour
 
         resolutionDropdown.ClearOptions();
 
+        int currentResolutionIndex = 0;
         List<string> options = new List<string>();
 
         for (int i = 0; i < resolutions.Length; i++) {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
+
+    public void setResolution(int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    
     public void SetVolume(float volume) {
         audioMixer.SetFloat("volume", volume);
     }
